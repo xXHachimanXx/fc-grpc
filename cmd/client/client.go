6a761e7c99a150e212334f2c8620c2b92edf86bf
xXHachimanXx <globaldep90@gmail.com>
2022-01-09 18:20:55 -0300

    Creating a test client

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 
+	"github.com/xXHachimanXx/fc-grpc/pb"
 	"google.golang.org/grpc"
 )
 
@@ -11,5 +14,24 @@ func main() {
 	if err != nil {
 		log.Fatalf("Could not connect to gRPC server: %v", err)
 	}
+	defer connection.Close()
 
+	client := pb.NewUserServiceClient(connection)
+	AddUser(client)
+
+}
+
+func AddUser(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "Juca",
+		Email: "jooj@jjj.com",
+	}
+
+	res, err := client.AddUser(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	fmt.Println(res)
 }
