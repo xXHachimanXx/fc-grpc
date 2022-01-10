ae342be30340fdd070ea62ea9281008b8c7df5ba
xXHachimanXx <globaldep90@gmail.com>
2022-01-10 09:46:38 -0300

    Implementing client for Server Stream

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/xXHachimanXx/fc-grpc/pb"
@@ -17,7 +18,8 @@ func main() {
 	defer connection.Close()
 
 	client := pb.NewUserServiceClient(connection)
-	AddUser(client)
+	// AddUser(client)
+	AddUserVerbose(client)
 
 }
 
@@ -34,4 +36,28 @@ func AddUser(client pb.UserServiceClient) {
 	}
 
 	fmt.Println(res)
+}
+
+func AddUserVerbose(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "Juca",
+		Email: "jooj@jjj.com",
+	}
+
+	responseStream, err := client.AddUserVerbose(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	for {
+		stream, err := responseStream.Recv()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatalf("Could not receive the msg: %v", err)
+		}
+		fmt.Println("Status: ", stream.Status)
+	}
+
 }
