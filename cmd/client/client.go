f249fe6b3cbd25574b0651cab7be2ae23a8c8e95
xXHachimanXx <globaldep90@gmail.com>
2022-01-10 18:15:00 -0300

    Creating client streaming

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"github.com/xXHachimanXx/fc-grpc/pb"
 	"google.golang.org/grpc"
@@ -19,7 +20,8 @@ func main() {
 
 	client := pb.NewUserServiceClient(connection)
 	// AddUser(client)
-	AddUserVerbose(client)
+	// AddUserVerbose(client)
+	AddUsers(client)
 
 }
 
@@ -59,5 +61,51 @@ func AddUserVerbose(client pb.UserServiceClient) {
 		}
 		fmt.Println("Status: ", stream.Status)
 	}
+}
 
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "1",
+			Name:  "Felipe",
+			Email: "felipe@email.com",
+		},
+		&pb.User{
+			Id:    "2",
+			Name:  "Felipe 2",
+			Email: "felipe2@email.com",
+		},
+		&pb.User{
+			Id:    "3",
+			Name:  "Felipe3",
+			Email: "felipe3@email.com",
+		},
+		&pb.User{
+			Id:    "4",
+			Name:  "Felipe4",
+			Email: "felipe4@email.com",
+		},
+		&pb.User{
+			Id:    "5",
+			Name:  "Felipe5",
+			Email: "felipe5@email.com",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error receiving response: %v", err)
+	}
+
+	fmt.Println(res)
 }
--- services/UserService.go
+++ services/UserService.go
@@ -3,6 +3,8 @@ package services
 import (
 	"context"
 	"fmt"
+	"io"
+	"log"
 	"time"
 
 	"github.com/xXHachimanXx/fc-grpc/pb"
@@ -66,4 +68,28 @@ func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVe
 	time.Sleep(time.Second * 3)
 
 	return nil
+}
+
+func (*UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
+	users := []*pb.User{}
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.Users{
+				User: users,
+			})
+		}
+		if err != nil {
+			log.Fatalf("Error receiving stream: %v", err)
+		}
+
+		users = append(users, &pb.User{
+			Id:    req.GetId(),
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		})
+
+		fmt.Println("Adding", req.GetId())
+	}
 }
